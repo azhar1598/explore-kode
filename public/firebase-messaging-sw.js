dbb6b0d29b24faf8a452a8f167a7b3ba0119fa5d
azhar1598 <mohammedazhar.1598@gmail.com>
2024-12-24 00:48:23 +0530

    changes

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,7 +3,16 @@ importScripts(
   "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
 );
 
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyAL7xitKquPEdjzfO5MRfnGieyVywEJYeA",
+  authDomain: "storekode-11006.firebaseapp.com",
+  projectId: "storekode-11006",
+  storageBucket: "storekode-11006.firebasestorage.app",
+  messagingSenderId: "282337359783",
+  appId: "1:282337359783:web:f6e83d06db5cc580252882",
+  measurementId: "G-7T9XP2G4PQ",
+};
+
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
